7ed396eeac92f091c60a4a299594e4f46e185231
Lucas Barbosa <Lucasbarbosasantos1920@gmail.com>
2025-11-06 09:56:42 -0300

    function

--- game.js
+++ game.js
@@ -1,8 +1,46 @@
 const config ={
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1340,
+    height: 630,
     physics:{
         default:"arcade",
+        arcade:{
+            gravity:{y:0},
+            debug:false
+        }
+    },
+    scene:{
+        preload:preload,
+        create:create,
+        update:update
     }
 }
+
+const game = new Phaser.Game(config);
+
+let player;
+let cursors;
+
+function preload(){
+    this.load.image("player",'Steve.png');
+}
+
+function create(){
+    player = this.physics.add.sprite(40,300,"player");
+    player.setScale(0.5);
+
+    cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update(){
+    if(cursors.left.isDown){
+        player.x -= 3;
+    }
+    else if(cursors.right.isDown){
+        player.x += 3;
+    }
+    else if (cursors.up.isDown){
+        player.y -= 3;
+    }
+}
+
